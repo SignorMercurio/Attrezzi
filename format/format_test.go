9c2d78761d74f07b7ab10738e82694c3b0d5bfca
Mercurio <10175101282@stu.ecnu.edu.cn>
2021-09-17 00:28:39 +0100

    Fix a bug in test code & Refactor tests

--- format/format_test.go
+++ format/format_test.go
@@ -7,6 +7,11 @@ import (
 	"github.com/SignorMercurio/attrezzi/cmd"
 )
 
+var (
+	in  = "./test/in.txt"
+	out = "./test/out.txt"
+)
+
 func exec(args ...string) {
 	rootCmd := cmd.NewRootCmd()
 	fmtCmd := NewFmtCmd()
@@ -19,11 +24,12 @@ func exec(args ...string) {
 	)
 	rootCmd.AddCommand(fmtCmd)
 
-	rootCmd.SetArgs(args)
+	commonArgs := []string{"fmt", "-o", out, "-i"}
+	rootCmd.SetArgs(append(commonArgs, args...))
 	rootCmd.Execute()
 }
 
-func checkResult(out string, expected string, t *testing.T) {
+func checkResult(expected string, t *testing.T) {
 	res, err := ioutil.ReadFile(out)
 	if err != nil {
 		t.Fatal(err)
@@ -36,126 +42,96 @@ func checkResult(out string, expected string, t *testing.T) {
 }
 
 func TestB64Encode(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "aGVsbG8gd29ybGQ="
-	exec("fmt", "-i", in, "-o", out, "b64", "-e")
-	checkResult(out, expected, t)
+	exec(in, "b64", "-e")
+	checkResult(expected, t)
 }
 
 func TestB64Decode(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "hello world"
-	exec("fmt", "-i", in, "-o", out, "b64", "-e")
-	exec("fmt", "-i", out, "-o", out, "b64", "-d")
-	checkResult(out, expected, t)
+	exec(in, "b64", "-e")
+	exec(out, "b64", "-d")
+	checkResult(expected, t)
 }
 
 func TestHexEncode(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "68656c6c6f20776f726c64"
-	exec("fmt", "-i", in, "-o", out, "hex", "-e")
-	checkResult(out, expected, t)
+	exec(in, "hex", "-e")
+	checkResult(expected, t)
 }
 
 func TestHexDecode(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "hello world"
-	exec("fmt", "-i", in, "-o", out, "hex", "-e")
-	exec("fmt", "-i", out, "-o", out, "hex", "-d")
-
-	checkResult(out, expected, t)
+	exec(in, "hex", "-e")
+	exec(out, "hex", "-d")
+	checkResult(expected, t)
 }
 
 func TestHexEncodeWith0x(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "0x680x650x6c0x6c0x6f0x200x770x6f0x720x6c0x64"
-	exec("fmt", "-i", in, "-o", out, "hex", "-e", "--delim", "0x", "-p")
-	checkResult(out, expected, t)
+	exec(in, "hex", "-e", "--delim", "0x", "-p")
+	checkResult(expected, t)
 }
 
 func TestHexDecodeWith0x(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "hello world"
-	exec("fmt", "-i", in, "-o", out, "hex", "-e", "--delim", "0x", "-p")
-	exec("fmt", "-i", out, "-o", out, "hex", "-d", "--delim", "0x", "-p")
-
-	checkResult(out, expected, t)
+	exec(in, "hex", "-e", "--delim", "0x", "-p")
+	exec(out, "hex", "-d", "--delim", "0x", "-p")
+	checkResult(expected, t)
 }
 
 func TestBinEncodeWithSpace(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100"
-	exec("fmt", "-i", in, "-o", out, "bin", "-e", "--delim", " ")
-	checkResult(out, expected, t)
+	exec(in, "bin", "-e", "--delim", " ")
+	checkResult(expected, t)
 }
 
 func TestBinDecodeWithSpace(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "hello world"
-	exec("fmt", "-i", in, "-o", out, "bin", "-e", "--delim", " ")
-	exec("fmt", "-i", out, "-o", out, "bin", "-d", "--delim", " ")
-
-	checkResult(out, expected, t)
+	exec(in, "bin", "-e", "--delim", " ")
+	exec(out, "bin", "-d", "--delim", " ")
+	checkResult(expected, t)
 }
 
 func TestDecEncodeWithLF(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
-	expected := "01101000\n01100101\n01101100\n01101100\n01101111\n00100000\n01110111\n01101111\n01110010\n01101100\n01100100"
-	exec("fmt", "-i", in, "-o", out, "bin", "-e", "--delim", "\\n")
-	checkResult(out, expected, t)
+	expected := "104\n101\n108\n108\n111\n32\n119\n111\n114\n108\n100\n"
+	exec(in, "dec", "-e", "--delim", "\\n")
+	checkResult(expected, t)
 }
 
 func TestDecDecodeWithLF(t *testing.T) {
-	in := "./test/in.txt"
-	out := "./test/out.txt"
 	expected := "hello world"
-	exec("fmt", "-i", in, "-o", out, "bin", "-e", "--delim", "\\n")
-	exec("fmt", "-i", out, "-o", out, "bin", "-d", "--delim", "\\n")
-
-	checkResult(out, expected, t)
+	exec(in, "dec", "-e", "--delim", "\\n")
+	exec(out, "dec", "-d", "--delim", "\\n")
+	checkResult(expected, t)
 }
 
 func TestURLEncode(t *testing.T) {
 	in := "./test/in_url.txt"
-	out := "./test/out.txt"
 	expected := `https://www.example.com/a/b/?c=d&e=f#g%E4%B8%AD%E6%96%87`
-	exec("fmt", "-i", in, "-o", out, "url", "-e")
-	checkResult(out, expected, t)
+	exec(in, "url", "-e")
+	checkResult(expected, t)
 }
 
 func TestURLDecode(t *testing.T) {
 	in := "./test/in_url.txt"
-	out := "./test/out.txt"
 	expected := "https://www.example.com/a/b/?c=d&e=f#g中文"
-	exec("fmt", "-i", in, "-o", out, "url", "-e")
-	exec("fmt", "-i", out, "-o", out, "url", "-d")
-
-	checkResult(out, expected, t)
+	exec(in, "url", "-e")
+	exec(out, "url", "-d")
+	checkResult(expected, t)
 }
 
 func TestURLEncodeAll(t *testing.T) {
 	in := "./test/in_url.txt"
-	out := "./test/out.txt"
 	expected := `https%3A%2F%2Fwww.example.com%2Fa%2Fb%2F%3Fc%3Dd%26e%3Df%23g%E4%B8%AD%E6%96%87`
-	exec("fmt", "-i", in, "-o", out, "url", "-ea")
-	checkResult(out, expected, t)
+	exec(in, "url", "-ea")
+	checkResult(expected, t)
 }
 
 func TestURLDecodeAll(t *testing.T) {
 	in := "./test/in_url.txt"
-	out := "./test/out.txt"
 	expected := "https://www.example.com/a/b/?c=d&e=f#g中文"
-	exec("fmt", "-i", in, "-o", out, "url", "-ea")
-	exec("fmt", "-i", out, "-o", out, "url", "-d")
-
-	checkResult(out, expected, t)
+	exec(in, "url", "-ea")
+	exec(out, "url", "-d")
+	checkResult(expected, t)
 }
