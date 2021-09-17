e267be8b3720d3d22b591ba7a17af852dc4d5d71
Mercurio <10175101282@stu.ecnu.edu.cn>
2021-09-17 20:51:51 +0100

    Refactor test code

--- format/format_test.go
+++ format/format_test.go
@@ -10,6 +10,7 @@ import (
 var (
 	in  = "./test/in.txt"
 	out = "./test/out.txt"
+	src = "hello world"
 )
 
 func exec(args ...string) {
@@ -41,97 +42,102 @@ func checkResult(expected string, t *testing.T) {
 	}
 }
 
-func TestB64Encode(t *testing.T) {
-	expected := "aGVsbG8gd29ybGQ="
+func TestB64(t *testing.T) {
+	dst := "aGVsbG8gd29ybGQ="
 	exec(in, "b64", "-e")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestB64Decode(t *testing.T) {
-	expected := "hello world"
-	exec(in, "b64", "-e")
 	exec(out, "b64", "-d")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestHexEncode(t *testing.T) {
-	expected := "68656c6c6f20776f726c64"
+func TestHex(t *testing.T) {
+	dst := "68656c6c6f20776f726c64"
 	exec(in, "hex", "-e")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestHexDecode(t *testing.T) {
-	expected := "hello world"
-	exec(in, "hex", "-e")
 	exec(out, "hex", "-d")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestHexEncodeWith0x(t *testing.T) {
-	expected := "0x680x650x6c0x6c0x6f0x200x770x6f0x720x6c0x64"
+func TestHexWith0x(t *testing.T) {
+	dst := "0x680x650x6c0x6c0x6f0x200x770x6f0x720x6c0x64"
 	exec(in, "hex", "-e", "--delim", "0x", "-p")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestHexDecodeWith0x(t *testing.T) {
-	expected := "hello world"
-	exec(in, "hex", "-e", "--delim", "0x", "-p")
 	exec(out, "hex", "-d", "--delim", "0x", "-p")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestBinEncodeWithSpace(t *testing.T) {
-	expected := "01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100"
+func TestBinWithSpace(t *testing.T) {
+	dst := "01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100"
 	exec(in, "bin", "-e", "--delim", " ")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestBinDecodeWithSpace(t *testing.T) {
-	expected := "hello world"
-	exec(in, "bin", "-e", "--delim", " ")
 	exec(out, "bin", "-d", "--delim", " ")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestDecEncodeWithLF(t *testing.T) {
-	expected := "104\n101\n108\n108\n111\n32\n119\n111\n114\n108\n100\n"
+func TestDecWithLF(t *testing.T) {
+	dst := "104\n101\n108\n108\n111\n32\n119\n111\n114\n108\n100\n"
 	exec(in, "dec", "-e", "--delim", "\\n")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestDecDecodeWithLF(t *testing.T) {
-	expected := "hello world"
-	exec(in, "dec", "-e", "--delim", "\\n")
 	exec(out, "dec", "-d", "--delim", "\\n")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestURLEncode(t *testing.T) {
+func TestURL(t *testing.T) {
 	in := "./test/in_url.txt"
-	expected := `https://www.example.com/a/b/?c=d&e=f#g%E4%B8%AD%E6%96%87`
+	src := "https://www.example.com/a/b/?c=d&e=f#g中文"
+	dst := `https://www.example.com/a/b/?c=d&e=f#g%E4%B8%AD%E6%96%87`
 	exec(in, "url", "-e")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestURLDecode(t *testing.T) {
-	in := "./test/in_url.txt"
-	expected := "https://www.example.com/a/b/?c=d&e=f#g中文"
-	exec(in, "url", "-e")
 	exec(out, "url", "-d")
-	checkResult(expected, t)
+	checkResult(src, t)
 }
 
-func TestURLEncodeAll(t *testing.T) {
+func TestURLAll(t *testing.T) {
 	in := "./test/in_url.txt"
-	expected := `https%3A%2F%2Fwww.example.com%2Fa%2Fb%2F%3Fc%3Dd%26e%3Df%23g%E4%B8%AD%E6%96%87`
+	src := "https://www.example.com/a/b/?c=d&e=f#g中文"
+	dst := `https%3A%2F%2Fwww.example.com%2Fa%2Fb%2F%3Fc%3Dd%26e%3Df%23g%E4%B8%AD%E6%96%87`
 	exec(in, "url", "-ea")
-	checkResult(expected, t)
-}
+	checkResult(dst, t)
 
-func TestURLDecodeAll(t *testing.T) {
-	in := "./test/in_url.txt"
-	expected := "https://www.example.com/a/b/?c=d&e=f#g中文"
-	exec(in, "url", "-ea")
 	exec(out, "url", "-d")
-	checkResult(expected, t)
+	checkResult(src, t)
+}
+
+func TestHTMLNamedAll(t *testing.T) {
+	in := "./test/in_html.txt"
+	src := "<script>alert('xss');</script>"
+	dst := `&lt;&#115;&#99;&#114;&#105;&#112;&#116;&gt;&#97;&#108;&#101;&#114;&#116;&lpar;&apos;&#120;&#115;&#115;&apos;&rpar;&semi;&lt;&sol;&#115;&#99;&#114;&#105;&#112;&#116;&gt;`
+	exec(in, "htm", "-ea")
+	checkResult(dst, t)
+
+	exec(out, "htm", "-d")
+	checkResult(src, t)
+}
+
+func TestHTMLDec(t *testing.T) {
+	in := "./test/in_html.txt"
+	src := "<script>alert('xss');</script>"
+	dst := `&#60;script&#62;alert&#40;&#39;xss&#39;&#41;&#59;&#60;&#47;script&#62;`
+	exec(in, "htm", "-e", "-t", "dec")
+	checkResult(dst, t)
+
+	exec(out, "htm", "-d", "-t", "dec")
+	checkResult(src, t)
+}
+
+func TestHTMLHex(t *testing.T) {
+	in := "./test/in_html.txt"
+	src := "<script>alert('xss');</script>"
+	dst := `&#x3c;script&#x3e;alert&#x28;&#x27;xss&#x27;&#x29;&#x3b;&#x3c;&#x2f;script&#x3e;`
+	exec(in, "htm", "-e", "-t", "hex")
+	checkResult(dst, t)
+
+	exec(out, "htm", "-d", "-t", "hex")
+	checkResult(src, t)
 }
