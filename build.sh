3203b21fec78cddead9e9cd35ff4cbaec26b454b
Mercurio <10175101282@stu.ecnu.edu.cn>
2021-09-26 13:34:09 +0100

    net/ips done

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Building attrezzi..."
 
-go build -trimpath -ldflags="-w -s" -gcflags "-N -l" -o bin/att
-GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-w -s" -gcflags "-N -l" -o bin/att_linux_x64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -trimpath -ldflags="-w -s" -gcflags "-N -l" -o bin/att
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-w -s" -gcflags "-N -l" -o bin/linux_amd64/att
---
+++ net/ips.go
@@ -0,0 +1,109 @@
+/*
+Copyright © 2021 SignorMercurio
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package net
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"net"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	cidr string
+)
+
+type CIDR struct {
+	ip    net.IP
+	ipnet *net.IPNet
+}
+
+func (c *CIDR) Mask() string {
+	mask, _ := hex.DecodeString(c.ipnet.Mask.String())
+	return net.IP(mask).String()
+}
+
+func (c *CIDR) BroadcastIP() string {
+	mask, network := c.ipnet.Mask, c.ipnet.IP
+	maskLen, networkLen := len(mask), len(network)
+	b := network
+
+	for i := 0; i < maskLen; i++ {
+		idx := networkLen - i - 1
+		maskIdx := maskLen - i - 1
+		b[idx] = network[idx] | ^mask[maskIdx]
+	}
+
+	return b.String()
+}
+
+func (c *CIDR) Count() *big.Int {
+	ones, bits := c.ipnet.Mask.Size()
+	return big.NewInt(0).Lsh(big.NewInt(1), uint(bits-ones))
+}
+
+func ParseCIDR(s string) (*CIDR, error) {
+	ip, inet, err := net.ParseCIDR(s)
+	if err != nil {
+		return nil, errors.Wrap(err, "parse CIDR")
+	}
+
+	return &CIDR{ip: ip, ipnet: inet}, nil
+}
+
+// NewIpsCmd represents the ips command
+func NewIpsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "ips",
+		Short: "Show IP ranges",
+		Long: `Show IP ranges
+Example:
+	att net ips --cidr 10.0.0.0/24`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cidr, err := ParseCIDR(cidr)
+			if err != nil {
+				return err
+			}
+
+			network := cidr.ipnet.IP.String()
+			s := fmt.Sprintf(`CIDR: %s
+Network: %s
+Mask: %s
+Range: %s - %s
+Numbers of IP: %d`,
+				cidr.ipnet.String(),
+				network,
+				cidr.Mask(),
+				network,
+				cidr.BroadcastIP(),
+				cidr.Count(),
+			)
+			fmt.Println(s)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&cidr, "cidr", "", "CIDR")
+
+	return cmd
+}
+
+func init() {
+	netCmd.AddCommand(NewIpsCmd())
+}
